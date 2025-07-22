66042c747429461bdde773a835f736438756abd8
AngelaPatarroyo <angela.patarroyo@hotmail.com>
2025-07-22 22:35:10 +0100

    updated routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const AuthController = require('../controllers/authController');
 
+// Routes
 router.post('/register', (req, res) => AuthController.register(req, res));
 router.post('/login', (req, res) => AuthController.login(req, res));
 
